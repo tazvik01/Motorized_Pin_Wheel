cc4e6f2d954470fca3a5bc0746dd8be4f54911a2
tazvik <40014658+tazvik01@users.noreply.github.com>
2023-09-06 22:55:57 -0600

    Update MOTORIZED PIN WHEEL.ino
    added comments

--- MOTORIZED PIN WHEEL.ino
+++ MOTORIZED PIN WHEEL.ino
@@ -1,26 +1,27 @@
+// Define a variable to store the switch state (LOW or HIGH)
 int switchstate = 0;
-const int switch_1 = 2;
-const int motorPin = 9;
+
+// Define constants for pin numbers
+const int switch_1 = 2;   // Pin connected to the switch
+const int motorPin = 9;   // Pin connected to the motor control
 
 void setup() {
-  // put your setup code here, to run once:
+  // Setup code to run once when the Arduino starts
 
-  pinMode(switch_1, INPUT);
-  pinMode(motorPin, OUTPUT);
-
+  pinMode(switch_1, INPUT); // Set the switch pin as an input
+  pinMode(motorPin, OUTPUT); // Set the motor pin as an output
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Main code to run repeatedly
 
+  // Read the state of the switch and store it in the switchstate variable
   switchstate = digitalRead(switch_1);
 
-
-
-  if(switchstate == HIGH){
-    digitalWrite(motorPin, HIGH);
-  }else{
-    digitalWrite(motorPin, LOW);
+  // Check if the switch state is HIGH (meaning it's pressed)
+  if (switchstate == HIGH) {
+    digitalWrite(motorPin, HIGH); // Turn on the motor (set motorPin to HIGH)
+  } else {
+    digitalWrite(motorPin, LOW); // Turn off the motor (set motorPin to LOW)
   }
-
 }
